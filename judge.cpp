9c58cf4e319a0371a94c3069895c23ca0df849cc
sheriyuo <451954765@qq.com>
2023-02-23 21:28:24 +0800

    Update unfinished judge.cpp

--- judge.cpp
+++ judge.cpp
@@ -8,7 +8,8 @@ Judge::Judge(QObject *parent) :
 
 Judge::~Judge()
 {
-    /*none*/
+    for(int i = 1; i <= blockCnt; i++)
+        chessBlock[i].clear();
 }
 
 void Judge::setPlayerRole(int player)
@@ -20,4 +21,36 @@ void Judge::setPlayerRole(int player)
 void Judge::init()
 {
     memset(board, 0, sizeof(board));
+    blockCnt = 0;
+}
+
+void Judge::UpdateCurStep(int x, int y)
+{
+    int allEmpty = 1;
+    for(int i = 0; i < 4; i++)
+    {
+        int xx = x + dx[i], yy = y + dy[i]; // 枚举相邻点
+        if(!IsEmpty(xx, yy)) allEmpty = 0;
+    }
+    board[x][y] = curPlayer ? playerRole : 2 - playerRole;
+    blockCnt ++;
+    chessBelong[x][y] = blockCnt;
+    chessBlock[blockCnt].push_back(std::make_pair(x, y));
+    // 新建棋子块
+
+    if(allEmpty == 1)
+        return;
+
+    for(int i = 0; i < 4; i++)
+    {
+        int xx = x + dx[i], yy = y + dy[i]; // 枚举相邻点
+        if(board[xx][yy] == board[x][y])
+            MergeBlock(chessBelong[x][y], chessBelong[xx][yy]);
+    }
+}
+
+void Judge::MergeBlock(int x, int y)
+{
+    if(chessBlock[x].size() < chessBlock[y].size()) std::swap(x, y);
+
 }
--- judge.h
+++ judge.h
@@ -14,6 +14,9 @@
 #define CUS_BLACK 0xEAACB8
 #define CUS_WHITE 0x7ACBF5
 
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
 class Judge : public QObject
 {
     Q_OBJECT
@@ -22,13 +25,25 @@ public:
     Judge (QObject *parent = nullptr);
     ~Judge();
     void init();
+    bool IsEmpty(int x, int y);
+    bool CheckVaild(int x, int y); // 判断(x,y)是否可以下棋
+    void UpdateCurStep(int x, int y); // 更新当前操作
 
     int playerRole; // -1->white 1->black
-    int board[CHESS_BOARD_SIZE + 2][CHESS_BOARD_SIZE + 2];
-    int clickedRow, clickedColumn;
+    int curPlayer; // 1->player 0->bot
 
 public slots:
     void setPlayerRole(int player);
+
+private:
+    void WriteCurStep(int x, int y); // 记录当前操作
+    void MergeBlock(int x, int y); // 启发式合并
+
+    int board[CHESS_BOARD_SIZE + 2][CHESS_BOARD_SIZE + 2]; // 当前棋盘状态
+    int chessBelong[CHESS_BOARD_SIZE + 2][CHESS_BOARD_SIZE + 2]; // 棋子属于的棋子块
+    int blockLiberty[(CHESS_BOARD_SIZE + 2) * (CHESS_BOARD_SIZE + 2)]; // 棋子块的气数
+    int blockCnt; // 棋子块个数
+    std::vector<std::pair<int, int> >chessBlock[(CHESS_BOARD_SIZE + 2) * (CHESS_BOARD_SIZE + 2)]; // 棋子块的编号
 };
 
 #endif // JUDGE_H
