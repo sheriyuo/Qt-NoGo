5fb2c266b6cb5f0cf9243ccdde4f3c9c0349f986
sheriyuo <451954765@qq.com>
2023-02-23 21:49:52 +0800

    Finished dsu part in judge.cpp

--- judge.cpp
+++ judge.cpp
@@ -26,31 +26,41 @@ void Judge::init()
 
 void Judge::UpdateCurStep(int x, int y)
 {
-    int allEmpty = 1;
-    for(int i = 0; i < 4; i++)
-    {
-        int xx = x + dx[i], yy = y + dy[i]; // 枚举相邻点
-        if(!IsEmpty(xx, yy)) allEmpty = 0;
-    }
-    board[x][y] = curPlayer ? playerRole : 2 - playerRole;
+    board[x][y] = curPlayer ? playerRole : -playerRole;
     blockCnt ++;
     chessBelong[x][y] = blockCnt;
+    blockLiberty[blockCnt] = 4;
     chessBlock[blockCnt].push_back(std::make_pair(x, y));
     // 新建棋子块
 
-    if(allEmpty == 1)
-        return;
-
     for(int i = 0; i < 4; i++)
     {
         int xx = x + dx[i], yy = y + dy[i]; // 枚举相邻点
+        if(board[xx][yy] == 0) continue;
         if(board[xx][yy] == board[x][y])
             MergeBlock(chessBelong[x][y], chessBelong[xx][yy]);
+        else
+        {
+            blockLiberty[chessBelong[x][y]] --;
+            blockLiberty[chessBelong[xx][yy]] --;
+        }
     }
 }
 
-void Judge::MergeBlock(int x, int y)
+void Judge::MergeBlock(int x, int y) // 启发式合并
 {
     if(chessBlock[x].size() < chessBlock[y].size()) std::swap(x, y);
+    blockLiberty[x] += blockLiberty[y] - 2; // 去掉合并的 2 个气
 
+    typedef std::pair<int, int> Item;
+    for(Item u : chessBlock[y])
+    {
+        chessBlock[x].push_back(u);
+        chessBelong[u.first][u.second] = x;
+    }
+    // 合并
+
+    blockLiberty[y] = 0;
+    chessBlock[y].clear();
+    // 清空
 }
